848f100911a3911cd1069507069b4a391003176c
Roman <usankin.roma@gmail.com>
2025-02-17 10:57:03 +0400

    добавил CRUD для продуктов

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import {
+  ArrayMinSize,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString({
+    message: 'Название обязательно',
+  })
+  @IsNotEmpty({ message: 'Название не может быть пустым' })
+  title: string;
+
+  @IsString({ message: 'Описание должно быть строкой' })
+  @MaxLength(256, { message: 'Описание не может превышать 256 символов' })
+  description: string;
+
+  @IsNumber({}, { message: 'Цена должна быть числом' })
+  @IsOptional()
+  price: number;
+
+  @IsString({
+    message: 'Укажите хотя бы одну картинку',
+    each: true,
+  })
+  @ArrayMinSize(1, { message: 'Должна быть хотя бы одна картинка' })
+  @IsNotEmpty({
+    each: true,
+    message: 'Путь к картинке не может быть пустым',
+  })
+  images: string[];
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    return this.prisma.product.create({
+      data: createProductDto,
+    });
   }
 
   findAll() {
-    return `This action returns all product`;
+    return this.prisma.product.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        components: true,
+      },
+    });
+
+    if (!product) throw new NotFoundException('Товар не найден');
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    return this.prisma.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    await this.findOne(id);
+
+    return this.prisma.product.delete({
+      where: { id },
+    });
   }
 }

5aa4aa513047c46ed78b3c37d1d1aa2b3870fcd0
Roman <usankin.roma@gmail.com>
2025-02-20 23:54:36 +0400

    добавил загрузку картинок в систему

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,16 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ComponentModule } from './component/component.module';
 import { ProductModule } from './product/product.module';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule, ComponentModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule,
+    ComponentModule,
+    ProductModule,
+    FileModule,
+  ],
 })
 export class AppModule {}

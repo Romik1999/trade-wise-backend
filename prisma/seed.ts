9c52db5941e3206e7469c75fb699be5dda5ded89
roman <usankin.roma@gmail.com>
2025-03-19 23:22:54 +0400

    Добавил сидер на компоненты добавил призму в зависимости

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { EnumUnitMeasure, PrismaClient } from '@prisma/client';
 import { hash } from 'argon2';
 
 const prisma = new PrismaClient();
@@ -13,6 +13,72 @@ async function main() {
       password: await hash('123456'),
     },
   });
+
+  const components = [
+    {
+      id: 'cm8gap6ez00000cjp0xur51kg',
+      title: 'Цепочка 2 мм',
+      price: 52,
+      remainder: 10,
+      unit_measure: 'METERS',
+      size: 100,
+    },
+    {
+      id: 'cm8gaq9hz00010cjpg3eh3jd2',
+      title: 'Подвеска сердце 8 мм вытянутое',
+      price: 150,
+      remainder: 20,
+      unit_measure: 'PIECES',
+      size: 200,
+    },
+    {
+      id: 'cm8gargh000020cjp4jwr0cin',
+      title: 'Пин золотой',
+      price: 45,
+      remainder: 30,
+      unit_measure: 'PIECES',
+      size: 300,
+    },
+    {
+      id: 'cm8gatr7w00030cjp23g47unw',
+      title: 'Кольцо соединительное',
+      price: 100,
+      remainder: 40,
+      unit_measure: 'PACKAGING',
+      size: 400,
+    },
+    {
+      id: 'cm8gav4hn00050cjpbqsjb1z2',
+      title: 'Замочек золотой 4мм',
+      price: 35,
+      remainder: 50,
+      unit_measure: 'PACKAGING',
+      size: 500,
+    },
+  ];
+
+  await Promise.all(
+    components.map(({ id, title, price, remainder, unit_measure, size }) =>
+      prisma.component.upsert({
+        where: { id },
+        update: {},
+        create: {
+          title,
+          description: `Description ${title.toLowerCase()}`,
+          price,
+          images: [''],
+          seller_link:
+            'https://www.ozon.ru/product/krossovki-nike-nike-1473285214/',
+          remainder,
+          unit_measure: unit_measure as EnumUnitMeasure,
+          length: size,
+          width: size,
+          height: size,
+          diameter: size,
+        },
+      }),
+    ),
+  );
 }
 
 main()
